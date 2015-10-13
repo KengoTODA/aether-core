2fa008a81f4233e2fcca082a6c7cd7bc21b8d428
Kengo TODA <skypencil@gmail.com>
2015-10-14 00:19:35 +0800

    replace org.apache.http.client.params.AuthPolicy

--- aether-transport-http/src/main/java/org/eclipse/aether/transport/http/AuthSchemePool.java
+++ aether-transport-http/src/main/java/org/eclipse/aether/transport/http/AuthSchemePool.java
@@ -13,7 +13,7 @@ package org.eclipse.aether.transport.http;
 import java.util.LinkedList;
 
 import org.apache.http.auth.AuthScheme;
-import org.apache.http.client.params.AuthPolicy;
+import org.apache.http.client.config.AuthSchemes;
 import org.apache.http.impl.auth.BasicScheme;
 
 /**
@@ -38,7 +38,7 @@ final class AuthSchemePool
         {
             authScheme = authSchemes.removeLast();
         }
-        else if ( AuthPolicy.BASIC.equalsIgnoreCase( schemeName ) )
+        else if ( AuthSchemes.BASIC.equalsIgnoreCase( schemeName ) )
         {
             authScheme = new BasicScheme();
         }
